a61a36a8952c5a6b248718036fd178c023f39447
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-31 16:17:41 +1000

    Javascript for performing site operations including voting.

---
+++ themes/zebra/js/zebra.js
@@ -0,0 +1,46 @@
+(function(w, Zebra, $, undefined) {
+    
+    $(function() {
+
+        $(".story-voting").on("click", "a", function(e) {
+
+            e.preventDefault();
+        });
+
+    });
+
+})(window, window.Zebra = window.Zebra || {}, jQuery);
+
+(function(w, Zebra, $, undefined) {
+
+    Zebra.Vote = function() {
+
+        var processing = false;
+
+        return {
+
+            up: function(story_id) {
+                if (base_url) {
+                    $.post(base_url + 'ajax/vote', { action: "upvote", story_id: story_id }, function(response) {
+                        return response;
+                    });
+                } else {
+                    alert('Base URL is missing!');
+                }
+            },
+
+            down: function(story_id) {
+                if (base_url) {
+                    $.post(base_url + 'ajax/vote', { action: "downvote", story_id: story_id }, function(response) {
+                        return response;
+                    });
+                } else {
+                    alert('Base URL is missing!');
+                }
+            }
+
+        }
+
+    }();
+
+})();})(window, window.Zebra = window.Zebra || {}, jQuery);
