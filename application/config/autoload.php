5ec624e165451079985f2b9373eb21fbd7c494d9
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-31 11:56:16 +1000

    Added in some routes and autoloading the auth library.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -79,7 +79,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'email', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'parser');
+$autoload['libraries'] = array('database', 'wolfauth', 'parser');
 
 
 /*
