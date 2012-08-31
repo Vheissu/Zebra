380f33551a8a5ddba56f3c057c9a92ecdb59cb1d
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-31 16:17:56 +1000

    AJAX controller performs all AJAX requests.

---
+++ application/controllers/ajax.php
@@ -0,0 +1,56 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ajax extends MY_Controller {
+
+    public function __construct()
+    {
+        // Story helper allows us to perform votes, etc
+        $this->load->helper('story/story');
+    }
+
+	public function index()
+	{
+		die('Access Denied');
+	}
+
+    /**
+     * Vote
+     * 
+     * Performs an upvote or downvote for a particular
+     * story based on its ID.
+     * 
+     */ 
+    public function vote()
+    {
+        // Result to return
+        $result "Invalid action or URL parameter(s).";
+
+        if ($this->input->post('action'))
+        {
+            $action   = $this->input->post('action');
+            $story_id = $this->input->post('story_id');
+            $reason   = $this->input->post('downvote_reason');
+
+            switch ($action)
+            {
+                case 'upvote':
+                    if (cast_vote('up', $story_id))
+                    {
+                        $result = "success|upvote|"$story_id"";
+                    }
+                break;
+
+                case 'downvote':
+                    if (cast_vote('down', $story_id, $reason))
+                    {
+                        $result = "success|downvote|"$story_id"";
+                    }
+                break;
+            }
+        }
+
+        die ($result);
+    }
+}
+
+/* End of file ajax.php */
