aa20df23209b8003d9d17f6b021c6b62b844b831
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-31 16:19:19 +1000

    Added in variables to send a reason for downvotes.

--- themes/zebra/js/zebra.js
+++ themes/zebra/js/zebra.js
@@ -29,9 +29,9 @@
                 }
             },
 
-            down: function(story_id) {
+            down: function(story_id, reason_id) {
                 if (base_url) {
-                    $.post(base_url + 'ajax/vote', { action: "downvote", story_id: story_id }, function(response) {
+                    $.post(base_url + 'ajax/vote', { action: "downvote", story_id: story_id, downvote_reason: reason_id }, function(response) {
                         return response;
                     });
                 } else {
