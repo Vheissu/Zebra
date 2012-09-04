383329f815ffe27f262070f4ceb834ca1a453eb4
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-09-05 08:31:32 +1000

    Documenting methods and making DB calls work for vote checking.

--- application/modules/vote/models/vote_model.php
+++ application/modules/vote/models/vote_model.php
@@ -4,6 +4,15 @@ class Vote_model extends MY_Model {
 
 	protected $_table = 'votes';
 
+    /**
+     * Get Downvote Reasons
+     * 
+     * Return a list of valid downvote reasons
+     * and ID's to display on the front-end.
+     * 
+     * @return object or boolean false
+     * 
+     */ 
     public function get_downvote_reasons()
     {
         $query = $this->db->get('vote_reasons');
@@ -11,6 +20,19 @@ class Vote_model extends MY_Model {
         return ($query->num_rows() >= 1) ? $query->result() : FALSE;
     }
 
+    /**
+     * User Has Upvoted Story
+     * 
+     * Will check if a user has upvoted a particular
+     * story item in the database.
+     * 
+     * @access public
+     * @param int $story_id
+     * @param int $user_id
+     * 
+     * @return boolean
+     * 
+     */ 
     public function user_has_upvoted_story($story_id, $user_id)
     {
         $this->db->where('story_id', $story_id);
@@ -22,13 +44,45 @@ class Vote_model extends MY_Model {
         return ($query->num_rows() >= 1) ? TRUE : FALSE;
     }
 
+    /**
+     * User Has Downvoted Story
+     * 
+     * Will check if a user has downvoted a particular
+     * story item in the database.
+     * 
+     * @access public
+     * @param int $story_id
+     * @param int $user_id
+     * 
+     * @return boolean
+     * 
+     */ 
     public function user_has_downvoted_story($story_id, $user_id)
     {
-        $query = $this->db->get_where($this->_table, array('story_id' => $story_id, 'user_id' => $user_id, 'vote_type' => 'downvote'));
+        $this->db->where('story_id', $story_id);
+        $this->db->where('user_id', $user_id);
+        $this->db->where('vote_type', 'downvote');
+
+        $query = $this->db->get($this->_table);
 
         return ($query->num_rows() >= 1) ? TRUE : FALSE;    
     }
 
+    /**
+     * Cast Story Vote
+     * 
+     * Will insert a vote record into the votes table
+     * if a user votes up or down a particular story
+     * 
+     * @access public
+     * @param string $type
+     * @param int $story_id
+     * @param int $user_id
+     * @param int $reason_id
+     * 
+     * @return boolean
+     * 
+     */ 
     public function cast_story_vote($type = "up", $story_id, $user_id, $reason_id = 0)
     {
     	if ($type == 'up')
