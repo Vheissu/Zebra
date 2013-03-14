29855fc1cfb30b6b6fd06170e34db711a7f909b1
Dwayne Charrington <dwayencharrington@gmail.com>
2013-03-14 12:59:29 +1000

    Fuck Backbone, lets roll our own simple implementation.

--- application/controllers/api/story.php
+++ application/controllers/api/story.php
@@ -5,11 +5,17 @@ class Story extends REST_Controller
     // Allows a story to be fetched
     public function story_get()
     {
+        $this->load->model('story/story_model', 'story_m');
+
         if (!$this->get('id'))
         {
             $this->response(NULL, 400);
         }
 
+        // Load in a story
+        $story = $this->story_m->get_story($this->get('id'));
+
+        // If we have a story
         if ($story)
         {
             // Return the story
@@ -22,6 +28,11 @@ class Story extends REST_Controller
         }
     }
 
+    public function debug_get()
+    {
+        var_dump($this->request->body);
+    }
+
     // A new story submission
     public function story_post()
     {
@@ -31,54 +42,6 @@ class Story extends REST_Controller
 
         $return = array('status' => '');
 
-        $this->response()
+        $this->response();
     }
-    
-    function user_post()
-    {
-        //$this->some_model->updateUser( $this->get('id') );
-        $message = array('id' => $this->get('id'), 'name' => $this->post('name'), 'email' => $this->post('email'), 'message' => 'ADDED!');
-        
-        $this->response($message, 200); // 200 being the HTTP response code
-    }
-    
-    function user_delete()
-    {
-    	//$this->some_model->deletesomething( $this->get('id') );
-        $message = array('id' => $this->get('id'), 'message' => 'DELETED!');
-        
-        $this->response($message, 200); // 200 being the HTTP response code
-    }
-    
-    function users_get()
-    {
-        //$users = $this->some_model->getSomething( $this->get('limit') );
-        $users = array(
-			array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com'),
-			array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com'),
-			3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => array('hobbies' => array('fartings', 'bikes'))),
-		);
-        
-        if($users)
-        {
-            $this->response($users, 200); // 200 being the HTTP response code
-        }
-
-        else
-        {
-            $this->response(array('error' => 'Couldn\'t find any users!'), 404);
-        }
-    }
-
-
-	public function send_post()
-	{
-		var_dump($this->request->body);
-	}
-
-
-	public function send_put()
-	{
-		var_dump($this->put('foo'));
-	}
 }
--- themes/zebra/views/layouts/layout.zebra.tpl
+++ themes/zebra/views/layouts/layout.zebra.tpl
@@ -58,7 +58,6 @@
 
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js" type="text/javascript"></script>
-    {js('backbone.js')}
     {js('zebra.js')}
 
     {block name=footer}{/block}
