3e456f0def47c0ba3be224aee65478553c774eef
Dwayne Charrington <dwayencharrington@gmail.com>
2013-03-14 16:46:01 +1000

    More functionality in the story API controller.

--- application/controllers/api/story.php
+++ application/controllers/api/story.php
@@ -2,11 +2,15 @@
 
 class Story extends REST_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('story/story_model', 'story_m');
+    }
+
     // Allows a story to be fetched
     public function story_get()
     {
-        $this->load->model('story/story_model', 'story_m');
-
         if (!$this->get('id'))
         {
             $this->response(NULL, 400);
@@ -28,9 +32,47 @@ class Story extends REST_Controller
         }
     }
 
-    public function debug_get()
+    public function stories_get()
     {
-        var_dump($this->request->body);
+        $type    = (string) (isset($this->get('type'))) ? $this->get('type') : FALSE;
+        $total   = (int) (isset($this->get('total'))) ? $this->get('total') : 50;
+        $offset  = (int) (isset($this->get('offset'))) ? $this->get('offset') : 0;
+        $user_id = (int) (isset($this->get('user_id'))) ? $this->get('user_id') : 0;
+
+        // Prevent totals from exceeding 100
+        if ($total > 100)
+        {
+            $total = 100;
+        }
+
+        if ($type == 'popular')
+        {
+            $stories = $this->story_m->get_popular_stories($total, $offset);
+        }
+        elseif ($type == 'new')
+        {
+            $stories = $this->story_m->get_new_stories($total, $offset);
+        }
+        elseif ($type == 'user')
+        {
+            $stories = $this->story_m->get_user_stories($total, $offset, $user_id);
+        }
+        else
+        {
+            $stories = $this->story_m->get_popular_stories($total, $offset);    
+        }
+
+        // If we have a story
+        if ($stories)
+        {
+            // Return the stories
+            $this->response($stories, 200);
+        }
+        else
+        {
+            // No stories found
+            $this->response(array('error' => 'There are no stories'), 404);
+        }
     }
 
     // A new story submission
